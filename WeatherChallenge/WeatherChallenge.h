02bd4c35f1d4e14f9c326d77083f8c01e72bb6fd
César González <cesar.gonzalez@ciberexperis.es>
2021-12-27 12:52:58 +0100

    Initial Commit

---
+++ WeatherChallenge/WeatherChallenge.h
@@ -0,0 +1,18 @@
+//
+//  WeatherChallenge.h
+//  WeatherChallenge
+//
+//  Created by César González Palomino on 27/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherChallenge.
+FOUNDATION_EXPORT double WeatherChallengeVersionNumber;
+
+//! Project version string for WeatherChallenge.
+FOUNDATION_EXPORT const unsigned char WeatherChallengeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherChallenge/PublicHeader.h>
+
+
