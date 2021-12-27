cba6f98a5a82d4d0dd61df81ae8a9d263676daa1
César González <cesar.gonzalez@ciberexperis.es>
2021-12-27 13:50:48 +0100

    add test target

---
+++ WeatherChallengeTests/LoadWeatherFromRemoteUseCaseTests.swift
@@ -0,0 +1,82 @@
+//
+//  LoadWeatherFromRemoteUseCaseTests.swift
+//  WeatherChallengeTests
+//
+//  Created by César González Palomino on 27/12/21.
+//
+
+import XCTest
+import WeatherChallenge
+
+final class RemoteWeatherLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+}
+
+class LoadWeatherFromRemoteUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!,
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: RemoteWeatherLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteWeatherLoader(url: url, client: client)
+        trackForMemoryLeaks(sut,file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
+        return (sut: sut,
+                client: client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURLs: [URL] {
+            messages.map { $0.url }
+        }
+        var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+        
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+            messages.append((url, completion))
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
+        }
+        
+        func complete(withStatusCode code: Int,
+                      data: Data,
+                      at index: Int = 0) {
+            let response = HTTPURLResponse(url: requestedURLs[index],
+                                           statusCode: code,
+                                           httpVersion: nil,
+                                           headerFields: nil)!
+            messages[index].completion(.success((data, response)))
+        }
+    }
+
+}
+
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject,
+                             file: StaticString = #filePath,
+                             line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance,
+                         "Instance should have been deallocated. Potential Memory leak",
+                         file: file,
+                         line: line)
+        }
+    }
+}
