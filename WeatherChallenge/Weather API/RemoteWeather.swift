8960af883b109379ff7bd8336d1a6d64373fbfd3
César González <cesar.gonzalez@ciberexperis.es>
2021-12-27 13:29:21 +0100

    creates URLSessionHTTPClient and RemoteWeather Item

---
+++ WeatherChallenge/Weather API/RemoteWeather.swift
@@ -0,0 +1,47 @@
+//
+//  RemoteWeather.swift
+//  WeatherChallenge
+//
+//  Created by César González on 27/12/21.
+//
+
+import Foundation
+
+// MARK: - RemoteWeather
+struct RemoteWeather: Codable {
+    let coord: Coord
+    let weather: [Weather]
+    let main: Main
+    let name: String
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lon, lat: Double
+}
+
+// MARK: - Main
+struct Main: Codable {
+    let temp, feelsLike, tempMin, tempMax: Double
+    let pressure, humidity: Int
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
+    }
+}
+
+// MARK: - Weather
+struct Weather: Codable {
+    let id: Int
+    let main, weatherDescription, icon: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, main
+        case weatherDescription = "description"
+        case icon
+    }
+}
---
+++ WeatherChallenge/Weather API/RemoteWeatherLoader.swift
@@ -0,0 +1,8 @@
+//
+//  RemoteWeatherLoader.swift
+//  WeatherChallenge
+//
+//  Created by César González Palomino on 27/12/21.
+//
+
+import Foundation
