a424291da6619b59f7417ffa580ee718a1b6e215
César González <cesar.gonzalez@ciberexperis.es>
2021-12-27 14:18:34 +0100

    update weather app model properties

--- WeatherChallenge/Weather Feature/WeatherLoader.swift
+++ WeatherChallenge/Weather Feature/WeatherLoader.swift
@@ -7,15 +7,30 @@
 
 import Foundation
 
-public protocol FeedLoader {
-    typealias Result = Swift.Result<[Weather], Error>
+public protocol WeatherLoader {
+    typealias Result = Swift.Result<Weather, Error>
     func load(completion: @escaping (Result) -> Void)
 }
 
 public struct Weather {
-    let latitude: Int
-    let longitude: Int
+    let latitude: Double
+    let longitude: Double
     let cityName: String
-    let temperature: String
-    let iconName: String
+    let temperature: Double
+    let description: String?
+    let iconName: String?
+    
+    public init(latitude: Double,
+                longitude: Double,
+                cityName: String,
+                temperature: Double,
+                description: String?,
+                iconName: String?) {
+        self.latitude = latitude
+        self.longitude = longitude
+        self.cityName = cityName
+        self.temperature = temperature
+        self.description = description
+        self.iconName = iconName
+    }
 }
